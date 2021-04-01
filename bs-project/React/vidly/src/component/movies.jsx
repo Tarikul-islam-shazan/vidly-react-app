779ec6d363c80afb1091c7cafffbff6ee83039a0
KM TARIKUL ISLAM <tarikulislam@users-MacBook-Pro.local>
2021-04-01 17:38:23 +0600

    Movies table refactor

--- bs-project/React/vidly/src/component/movies.jsx
+++ bs-project/React/vidly/src/component/movies.jsx
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import { getMovies } from '../services/fakeMovieService';
-import { getGenres, genres } from '../services/fakeGenreService';
+import { getGenres } from '../services/fakeGenreService';
 import { paginate } from '../utils/paginate';
-import Like from './common/like';
 import Pagination from './common/pagination';
 import ListGroup from './common/listGroup';
+import MoviesTable from './moviesTable';
 
 class Movies extends Component {
 
@@ -64,42 +64,11 @@ class Movies extends Component {
                     </div>
                     <div className="col">
                         <p>Showing {filtered.length} movies in database.</p>
-                        <table className="table">
-                            <thead>
-                                <tr>
-                                    <th>Title</th>
-                                    <th>Genre</th>
-                                    <th>Stock</th>
-                                    <th>Rate</th>
-                                    <th>Like</th>
-                                    <th>Delete</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {movies.map( movie => (
-                                    <tr key={movie._id}>
-                                        <td>{movie.title}</td>
-                                        <td>{movie.genre.name}</td>
-                                        <td>{movie.numberInStock}</td>
-                                        <td>{movie.dailyRentalRate}</td>
-                                        <td>
-                                            <Like 
-                                                onClick={() => this.handleLike(movie)} 
-                                                liked={movie.liked}
-                                            />
-                                        </td>
-                                        <td>
-                                            <button 
-                                                onClick={()=> this.handleDelete(movie)} 
-                                                className="btn btn-danger btn-sm"
-                                            >
-                                                Delete
-                                            </button>
-                                        </td>
-                                    </tr>
-                                ))}
-                            </tbody>
-                        </table>
+                        <MoviesTable
+                            movies={movies} 
+                            onLike={this.handleLike} 
+                            onDelete={this.handleDelete}
+                        />
                         <Pagination 
                             itemsCount={filtered.length} 
                             pageSize={pageSize} 
---
+++ bs-project/React/vidly/src/component/moviesTable.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import Like from './common/like';
+
+const MoviesTable = (props) => {
+    const { movies, onDelete, onLike } = props;
+    return (            
+    <table className="table">
+        <thead>
+            <tr>
+                <th>Title</th>
+                <th>Genre</th>
+                <th>Stock</th>
+                <th>Rate</th>
+                <th>Like</th>
+                <th>Delete</th>
+            </tr>
+        </thead>
+        <tbody>
+            { movies.map( movie => (
+                <tr key={movie._id}>
+                    <td>{movie.title}</td>
+                    <td>{movie.genre.name}</td>
+                    <td>{movie.numberInStock}</td>
+                    <td>{movie.dailyRentalRate}</td>
+                    <td>
+                        <Like 
+                            onClick={() => onLike(movie)} 
+                            liked={movie.liked}
+                        />
+                    </td>
+                    <td>
+                        <button 
+                            onClick={()=> onDelete(movie)} 
+                            className="btn btn-danger btn-sm"
+                        >
+                            Delete
+                        </button>
+                    </td>
+                </tr>
+            ))}
+        </tbody>
+    </table> 
+    );
+}
+ 
+export default MoviesTable;
