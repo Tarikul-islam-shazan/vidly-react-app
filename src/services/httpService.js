684ab57d7c42f0f6fbc59188bd243136c9878ebc
Tarikul-islam-shazan <Tarikul@brainstation23.com>
2022-03-15 17:43:20 +0600

    remove bi-directional dependencues from auth and http service

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -1,9 +1,6 @@
 import axios from "axios";
 import logger from "./logService";
-import auth from './authService';
 import { toast } from "react-toastify";
-
-axios.defaults.headers.common['x-auth-token'] = auth.getJwt();
 
 axios.interceptors.response.use(null,error => {
     const expectedError = error.response &&
@@ -17,10 +14,15 @@ axios.interceptors.response.use(null,error => {
     return Promise.reject(error);
 });
 
+function setJwt(jwt) {
+    axios.defaults.headers.common["x-auth-token"] = jwt;
+}
+
 // eslint-disable-next-line import/no-anonymous-default-export
 export  default {
     get: axios.get,
     post: axios.post,
     put: axios.put,
-    delete: axios.delete
+    delete: axios.delete,
+    setJwt
 }
