f71809eda881971e1e124fa5c307ee8ca49fb296
Tarikul-islam-shazan <Tarikul@brainstation23.com>
2022-03-31 15:53:22 +0600

    protected route update

--- src/component/loginForm.jsx
+++ src/component/loginForm.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import Joi from 'joi-browser';
 import Form from './common/form';
-import { login } from '../services/authService';
+import auth from '../services/authService';
+import { Redirect } from 'react-router-dom';
 
 class LoginForm extends Form {
     state = {
@@ -21,8 +22,9 @@ class LoginForm extends Form {
     doSubmit = async () => {
         try{
             const { data } = this.state;
-            await login(data.username, data.password);
-            window.location = '/';
+            await auth.login(data.username, data.password);
+            const { state } = this.props.location;
+            window.location = state ? state.from.pathname : '/';
         } catch(ex) {
             if(ex.response && ex.response.status === 400){
                 const errors = {...this.state.errors};
@@ -33,6 +35,7 @@ class LoginForm extends Form {
     }
 
     render() { 
+        if(auth.getCurrentUser()) return <Redirect to="/"/>
         return (
             <div>
                 <h1>Login</h1>
